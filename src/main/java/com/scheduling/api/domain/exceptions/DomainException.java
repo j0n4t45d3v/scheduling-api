daf13003a70c7fb61277ca5fe21a377436a5d28b
Jonatas <delimajonatas875@gmail.com>
2025-12-25 10:41:37 -0300

    feat: DomainException

---
+++ src/main/java/com/scheduling/api/domain/exceptions/DomainException.java
@@ -0,0 +1,9 @@
+package com.scheduling.api.domain.exceptions;
+
+public class DomainException extends RuntimeException{
+
+    public DomainException(String message) {
+        super(message);
+    }
+
+}
