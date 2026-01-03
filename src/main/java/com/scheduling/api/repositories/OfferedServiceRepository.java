41a8898f5a42ae582bed01345016f9eccc88cf4f
Jonatas <delimajonatas875@gmail.com>
2026-01-03 11:25:39 -0300

    feat: add findByName method in OfferedServiceRepository

--- src/main/java/com/scheduling/api/repositories/OfferedServiceRepository.java
+++ src/main/java/com/scheduling/api/repositories/OfferedServiceRepository.java
@@ -3,5 +3,8 @@ package com.scheduling.api.repositories;
 import com.scheduling.api.domain.OfferedService;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OfferedServiceRepository extends JpaRepository<OfferedService, Long> {
+    Optional<OfferedService> findByName(String name);
 }
